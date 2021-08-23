75a41a24952796aa01ed6aa5a56391983de61401
toliveira90 <34128530+toliveira90@users.noreply.github.com>
2021-08-23 15:54:59 +0100

    Created APP repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tatiana Oliveira and is {" "}
+        <a href="https://github.com/toliveira90/homework2"
         >
-          Learn React
+        open-sourced on GitbHub
         </a>
-      </header>
+      </footer>
     </div>
+
   );
 }
 
