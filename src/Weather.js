aceee39e6c8c45002838a5a9a385130bf65cd84c
toliveira90 <34128530+toliveira90@users.noreply.github.com>
2021-08-24 18:27:40 +0100

    adding axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+// eslint-disable-next-line 
+import axios from "axios";
+
 export default function Weather() {
-  return <div className="Weather">
-    <form>
-      <div className="row">
-        <div className="col-9">
-          <input
-            type="search"
-            placeholder="Enter a city..."
-            className="form-control"
-            autoFocus="on" />
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      date: "Wednesday 07:00",
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      humidity: response.data.main.humidity,
+      wind: response.data.main.speed,
+      city: response.data.name
+      
+
+    })
+    setReady(true);
+
+  }
+  if (weatherData.ready) {
+    
+    return <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on" />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary w100" />
+          </div>
         </div>
-        <div className="col-3">
-          <input type="submit" value="Search" className="btn btn-primary w100" />
+      </form>
+      <h1>{weatherData.city}</h1>
+      <ul>
+        <li>{weatherData.date}</li>
+        <li className="text-capitalize">{weatherData.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix"></div>
+          <img src={weatherData.iconUrl}
+          alt={weatherData.description}></img>
+          <div className="float-left"></div>
+          <span className="temperature">{Math.round(weatherData.temperature)}</span>
+          <span className="unit">°C</span>
         </div>
-      </div>
-    </form>
-    <h1>Barcelona</h1>
-    <ul>
-      <li>Wednesday 07:00</li>
-      <li>Sunny</li>
-    </ul>
-    <div className="row mt-3">
-      <div className="col-6">
-        <div className="clearfix"></div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" 
-        className="float-left"/>
-        <div className="float-left"></div>
-        <span className="temperature">27</span>
-        <span className="unit">°C</span>
+        <div className="col-6">
+          <ul>
+            <li> Precipitation: 10%</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}km/h</li>
+          </ul>
         </div>
-      <div className="col-6">
-        <ul>
-          <li> Precipitation: 10%</li>
-          <li> Humidity: 80%</li>
-          <li> Wind: 4km/h</li>
-        </ul>
       </div>
     </div>
-  </div>
 
 
 
 
+  } else {
+    const apiKey = "a6105b25a2bdbe419f9a7bbe7361c93e";
+    let city = "Porto";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metrics`;
+    axios.get(apiUrl).then(handleResponse)
+return "Loading...";
+}
 }
