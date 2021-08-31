f460b3245e162fd5a6f4f4c741a6c6881a31e172
toliveira90 <34128530+toliveira90@users.noreply.github.com>
2021-08-31 14:56:35 +0100

    Update Weather JS - HTTPS

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "a6105b25a2bdbe419f9a7bbe7361c93e";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse)
 
   }
